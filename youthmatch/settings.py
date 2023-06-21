d91a250275044fc957e83bce2982af23aec6da62
istoofeed <dakmood@gmail.com>
2023-06-22 01:51:16 +0800

    new

--- youthmatch/settings.py
+++ youthmatch/settings.py
@@ -84,16 +84,16 @@ WSGI_APPLICATION = 'youthmatch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
         #mysql localhost
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'testin',
-        # 'USER': 'root',
-        # 'PASSWORD': '',
-        # 'HOST': 'localhost',
-        # 'PORT': '3306'
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'youthmatch',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
